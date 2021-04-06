b7a1c315390f75dcadabb1d2eb9ae453196e0f6a
Charles Hall <chall8001@gmail.com>
2021-04-06 06:27:20 -0600

    added hashing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch, HashRouter } from 'react-router-dom'
 import Nav from './components/Navbar.js'
 import Footer from './components/Footer.js'
 import About from './components/About.js'
@@ -11,14 +11,14 @@ function App()  {
 
   return (
     <div>
-    <Router>
+   <HashRouter>
     <Nav />
     <Switch>
     <Route path="/projects" component={Project} />
         <Route path="/contact" component={Contact} />
         <Route path="/" component={About} />
         </Switch>
-    </Router>
+   </HashRouter>
 <Footer />
     </div>
 
